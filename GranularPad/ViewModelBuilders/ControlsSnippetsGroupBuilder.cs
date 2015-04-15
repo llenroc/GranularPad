dc00b2d90dba5a415466acbb2802e676eaf38547
YuvalT <yuvaltze@gmail.com>
2015-04-16 00:56:13 +0300

    Snippets - Controls - ProgressBar

--- GranularPad/ViewModelBuilders/ControlsSnippetsGroupBuilder.cs
+++ GranularPad/ViewModelBuilders/ControlsSnippetsGroupBuilder.cs
@@ -16,6 +16,7 @@ namespace GranularPad.ViewModelBuilders
                 new SnippetViewModel("Buttons", Granular.Compatibility.String.FromByteArray(EmbeddedResourceLoader.LoadResourceData(@"/GranularPad;component/Snippets/Controls.Buttons.xaml"))),
                 new SnippetViewModel("Image", Granular.Compatibility.String.FromByteArray(EmbeddedResourceLoader.LoadResourceData(@"/GranularPad;component/Snippets/Controls.Image.xaml"))),
                 new SnippetViewModel("Popup", Granular.Compatibility.String.FromByteArray(EmbeddedResourceLoader.LoadResourceData(@"/GranularPad;component/Snippets/Controls.Popup.xaml"))),
+                new SnippetViewModel("ProgressBar", Granular.Compatibility.String.FromByteArray(EmbeddedResourceLoader.LoadResourceData(@"/GranularPad;component/Snippets/Controls.ProgressBar.xaml"))),
             });
         }
     }
