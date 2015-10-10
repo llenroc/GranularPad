dfa4a348260b6306f655955f353d45be673ab4b2
YuvalT <yuvaltze@gmail.com>
2015-10-10 22:15:24 +0300

    Snippets - KeyboardNavigation

--- GranularPad/ViewModelBuilders/FrameworkSnippetsGroupBuilder.cs
+++ GranularPad/ViewModelBuilders/FrameworkSnippetsGroupBuilder.cs
@@ -17,6 +17,7 @@ namespace GranularPad.ViewModelBuilders
                 new SnippetViewModel("Binding Modes", Granular.Compatibility.String.FromByteArray(EmbeddedResourceLoader.LoadResourceData(@"/GranularPad;component/Snippets/Framework.Binding.Modes.xaml"))),
                 new SnippetViewModel("Binding RelativeSource", Granular.Compatibility.String.FromByteArray(EmbeddedResourceLoader.LoadResourceData(@"/GranularPad;component/Snippets/Framework.Binding.RelativeSource.xaml"))),
                 new SnippetViewModel("Binding UpdateTrigger", Granular.Compatibility.String.FromByteArray(EmbeddedResourceLoader.LoadResourceData(@"/GranularPad;component/Snippets/Framework.Binding.UpdateSourceTrigger.xaml"))),
+                new SnippetViewModel("KeyboardNavigation", Granular.Compatibility.String.FromByteArray(EmbeddedResourceLoader.LoadResourceData(@"/GranularPad;component/Snippets/Framework.KeyboardNavigation.xaml"))),
             });
         }
     }
